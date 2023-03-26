c65bf0fdd03d8cca40e25d63189dced0f3f8398b
Samuel Khongthaw <skthaw1@gmail.com>
2023-03-26 19:14:55 +0530

    Added logout button + minor ui refactorings

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
-import "@fontsource/montserrat/800.css"
+import '@/styles/globals.css';
+import "@fontsource/montserrat/800.css";
+import "@fontsource/montserrat/700.css";
+import "@fontsource/montserrat/600.css";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/400.css";
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- pages/login.js
+++ pages/login.js
@@ -3,7 +3,7 @@ function Login(){
         <main className="bg-black h-screen flex flex-col items-center justify-start">
             <div className="pt-20 max-w-lg flex flex-col justify-start items-start">
                 <div className="flex flex-col">
-                    <div className="text-gray-300 font-montserrat text-4xl">Log In</div>
+                    <div className="text-gray-300 font-montserrat text-4xl font-extrabold">Log In</div>
                     <div className="text-neutral-500 font-medium text-xs pt-10 pb-6">Enter you account details under.</div>
                     <div className="text-neutral-400 text-xs font-medium pb-2 placeholder:text-neutral-300" placeholder="Enter your username">Username</div>
                     <input placeholder="Enter your username" className="placeholder:text-neutral-600 max-w-full bg-neutral-900 rounded-md w-72 h-12 border-2 border-neutral-800 focus:ring-yellow-600 focus:ring-2 focus:outline-none text-gray-300 font-bold text-sm p-2"></input>
@@ -13,7 +13,7 @@ function Login(){
                     <div className="p-4 rounded-xl mt-8 bg-gradient-to-r duration-300 hover:rounded-md bg-opacity-50 from-yellow-700 via-yellow-600 to-yellow-400">
                         <div className="font-extrabold text-neutral-200 text-center cursor-pointer">Log In</div>
                     </div>
-                    <div className="text-neutral-500  font-bold text-center pt-4 text-xs">Don't have an account? <span className="text-neutral-200">Sign Up</span></div>
+                    <div className="text-neutral-600  font-medium text-center pt-4 text-xs">Account doesn't exist? <span className="text-neutral-200 cursor-pointer text-xs">Contact Admin.</span></div>
                 </div>
             </div>
         </main>
