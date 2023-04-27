987f44f103dca044c29cd8daddd6633c9c3215ab
Samuel Khongthaw <skthaw1@gmail.com>
2023-04-27 19:05:55 +0530

    fixed search not highlighting

--- pages/admin/add-student-modal.js
+++ pages/admin/add-student-modal.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
-
 import style from "./modal.module.css";
 import { STUDENTS, TEST_URL } from "../utils/constants";
 
@@ -8,9 +7,12 @@ function AddStudentModal({ show, onClose }) {
   const [isBrowser, setIsBrowser] = useState(false);
   const [fetching, setFetching] = useState(true);
   const [students, setStudents] = useState(null);
+  const [filteredStudents, setFilteredStudents] = useState(null);
+  const [selectedStudents, setSelectedStudents] = useState([]);
+
   useEffect(() => {
     setIsBrowser(true);
-    getStudents(setFetching, setStudents, students);
+    getStudents(setFetching, setStudents, students, setFilteredStudents);
   }, []);
 
   if (isBrowser) {
@@ -24,26 +26,83 @@ function AddStudentModal({ show, onClose }) {
             >
               <div className="bg-neutral-900 w-96 h-96 p-3 border-neutral-800 border-solid border-x-2 border-y-2">
                 <input
+                  onChange={(e) => {
+                    searchStudent(
+                      e.target.value,
+                      filteredStudents,
+                      students,
+                      setFilteredStudents
+                    );
+                  }}
                   placeholder="Search"
-                  className="placeholder:text-neutral-600 max-w-full bg-neutral-900 rounded-md w-full h-10 border-2 border-neutral-800 focus:ring-yellow-400 focus:ring-2 focus:outline-none text-gray-300 font-regular text-sm p-2"
+                  className="placeholder:text-neutral-600 mb-2 max-w-full bg-neutral-900 rounded-md w-full h-10 border-2 border-neutral-800 focus:ring-yellow-400 focus:ring-2 focus:outline-none text-gray-300 font-regular text-sm p-2"
                 ></input>
                 {/* <div>hello</div> */}
-                {students !== null ? (
-                  students[0].map((student) => {
-                    return (
-                      <>
-                        <div className="font-bold text-sm pt-2">
-                          {student.personName}
+                <div className="h-64  overflow-y-scroll">
+                  {filteredStudents !== null ? (
+                    filteredStudents[0].map((student) => {
+                      return (
+                        <div
+                          onClick={() => {
+                            if (!selectedStudents.includes(student.userId)) {
+                              selectedStudents.push(student.userId);
+                            } else {
+                              selectedStudents.splice(
+                                selectedStudents.indexOf(student.userId),
+                                1
+                              );
+                            }
+                            let newSelectedStudents = [...selectedStudents];
+                            setFilteredStudents(filteredStudents);
+                            setSelectedStudents(newSelectedStudents);
+                          }}
+                          key={student.userId}
+                          className={`${
+                            selectedStudents.includes(student.userId)
+                              ? "bg-yellow-500 hover:bg-red-500"
+                              : "hover:bg-neutral-800"
+                          } m-1 rounded-md p-2 cursor-pointer duration-200`}
+                        >
+                          <div>
+                            <div
+                              className={`font-bold text-sm pt-1 ${
+                                selectedStudents.includes(student.userId)
+                                  ? "text-black"
+                                  : "text-white"
+                              }`}
+                            >
+                              {student.personName}
+                            </div>
+                            <div
+                              className={`text-xs ${
+                                selectedStudents.includes(student.userId)
+                                  ? "text-black"
+                                  : "text-neutral-700"
+                              }`}
+                            >
+                              {student.userId}
+                            </div>
+                          </div>
                         </div>
-                        <div className="text-xs text-neutral-700 border-b-2 border-neutral-800">
-                          {student.userId}
-                        </div>
-                      </>
-                    );
-                  })
-                ) : (
-                  <></>
-                )}
+                      );
+                    })
+                  ) : (
+                    <div>No data</div>
+                  )}
+                </div>
+                <div className="flex justify-end mt-5">
+                  <div
+                    onClick={() => {
+                      onClose();
+                    }}
+                    className="text-red-400 font-bold text-xs pr-4 cursor-pointer hover:text-red-600 duration-200"
+                  >
+                    CANCEL
+                  </div>
+                  <div className="text-yellow-400 font-bold text-xs pr-4 cursor-pointer hover:text-yellow-600 duration-200">
+                    DONE
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -71,7 +130,13 @@ function AddStudentModal({ show, onClose }) {
   }
 }
 
-async function getStudents(setFetching, setStudents, students) {
+async function getStudents(
+  setFetching,
+  setStudents,
+  students,
+  setFilteredStudents
+) {
+  //if (students != null) return;
   setFetching(true);
   try {
     const response = await fetch(TEST_URL + STUDENTS, {
@@ -80,11 +145,33 @@ async function getStudents(setFetching, setStudents, students) {
     });
     const data = await response.json();
     setStudents([data]);
-    console.log(students);
+    setFilteredStudents([data]);
   } catch (e) {
     console.log("Error: ", e);
   }
   setFetching(false);
+}
+
+function searchStudent(
+  searchText,
+  filteredStudents,
+  students,
+  setFilteredStudents
+) {
+  if (searchText == "") {
+    setFilteredStudents(students);
+    return;
+  }
+  filteredStudents = [];
+  students.forEach((student) => {
+    if (
+      student.personName.toLowerCase().includes(searchText.toLowerCase()) ||
+      student.userId.toLowerCase().includes(searchText.toLowerCase())
+    ) {
+      filteredStudents.push(student);
+    }
+  });
+  setFilteredStudents(filteredStudents);
 }
 
 export default AddStudentModal;
