2b7b6c9faacb29a0729edad1806aa02e1a583b51
Samuel Khongthaw <skthaw1@gmail.com>
2023-04-28 09:26:51 +0530

    add faculty modal

---
+++ pages/admin/add-faculty-modal.js
@@ -0,0 +1,188 @@
+import React, { useContext, useEffect, useRef, useState } from "react";
+import ReactDOM from "react-dom";
+import style from "./modal.module.css";
+import { FACULTIES, TEST_URL } from "../utils/constants";
+import { FacultiesDispatchContext } from "../utils/contexts";
+
+function AddFacultyModal({ show, onClose }) {
+  const [isBrowser, setIsBrowser] = useState(false);
+  const [fetching, setFetching] = useState(true);
+  const [faculties, setFaculties] = useState(null);
+  const [filteredStudents, setFilteredFaculties] = useState(null);
+  const [selectedFaculties, setSelectedFaculties] = useState([]);
+  const dispatch = useContext(FacultiesDispatchContext);
+
+  useEffect(() => {
+    setIsBrowser(true);
+    getStudents(setFetching, setFaculties, faculties, setFilteredFaculties);
+  }, []);
+
+  if (isBrowser) {
+    if (show === true) {
+      return ReactDOM.createPortal(
+        !fetching ? (
+          <div className={style.font}>
+            <div
+              className="absolute top-0 left-0 w-screen h-screen flex justify-center items-center backdrop-blur-md "
+              // onClick={() => onClose()}
+            >
+              <div className="bg-neutral-900 w-96 h-96 p-3 border-neutral-800 border-solid border-x-2 border-y-2">
+                <input
+                  onChange={(e) => {
+                    searchStudent(
+                      e.target.value,
+                      filteredStudents,
+                      faculties,
+                      setFilteredFaculties
+                    );
+                  }}
+                  placeholder="Search"
+                  className="placeholder:text-neutral-600 mb-2 max-w-full bg-neutral-900 rounded-md w-full h-10 border-2 border-neutral-800 focus:ring-yellow-400 focus:ring-2 focus:outline-none text-gray-300 font-regular text-sm p-2"
+                ></input>
+                {/* <div>hello</div> */}
+                <div className="h-64  overflow-y-scroll">
+                  {filteredStudents !== null ? (
+                    filteredStudents[0].map((student) => {
+                      return (
+                        <div
+                          onClick={() => {
+                            if (!selectedFaculties.includes(student.userId)) {
+                              dispatch({
+                                type: "add",
+                                userId: student.userId,
+                                personName: student.personName,
+                              });
+                              selectedFaculties.push(student.userId);
+                            } else {
+                              selectedFaculties.splice(
+                                selectedFaculties.indexOf(student.userId),
+                                1
+                              );
+                              dispatch({
+                                type: "remove",
+                                userId: student.userId,
+                              });
+                            }
+                            let newSelectedStudents = [...selectedFaculties];
+                            setFilteredFaculties(filteredStudents);
+                            setSelectedFaculties(newSelectedStudents);
+                          }}
+                          key={student.userId}
+                          className={`${
+                            selectedFaculties.includes(student.userId)
+                              ? "bg-yellow-500 hover:bg-red-500"
+                              : "hover:bg-neutral-800"
+                          } m-1 rounded-md p-2 cursor-pointer duration-200`}
+                        >
+                          <div>
+                            <div
+                              className={`font-bold text-sm pt-1 ${
+                                selectedFaculties.includes(student.userId)
+                                  ? "text-black"
+                                  : "text-white"
+                              }`}
+                            >
+                              {student.personName}
+                            </div>
+                            <div
+                              className={`text-xs ${
+                                selectedFaculties.includes(student.userId)
+                                  ? "text-black"
+                                  : "text-neutral-700"
+                              }`}
+                            >
+                              {student.userId}
+                            </div>
+                          </div>
+                        </div>
+                      );
+                    })
+                  ) : (
+                    <div>No data</div>
+                  )}
+                </div>
+                <div className="flex justify-end mt-5">
+                  <div
+                    onClick={() => {
+                      onClose();
+                    }}
+                    className="text-red-400 font-bold text-xs pr-4 cursor-pointer hover:text-red-600 duration-200"
+                  >
+                    CANCEL
+                  </div>
+                  <div className="text-yellow-400 font-bold text-xs pr-4 cursor-pointer hover:text-yellow-600 duration-200">
+                    DONE
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className={style.font}>
+            <div
+              className="absolute top-0 left-0 w-screen h-screen flex justify-center items-center backdrop-blur-md "
+              // onClick={() => onClose()}
+            >
+              <div className="bg-neutral-900 w-96 h-96 p-3 border-neutral-800 border-solid border-x-2 border-y-2 flex justify-center items-center">
+                <div className="font-bold text-sm">
+                  Fetching faculty data...
+                </div>
+              </div>
+            </div>
+          </div>
+        ),
+        document.getElementById("modal")
+      );
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+}
+
+async function getStudents(
+  setFetching,
+  setStudents,
+  students,
+  setFilteredStudents
+) {
+  //if (students != null) return;
+  setFetching(true);
+  try {
+    const response = await fetch(TEST_URL + FACULTIES, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await response.json();
+    setStudents([data]);
+    setFilteredStudents([data]);
+  } catch (e) {
+    console.log("Error: ", e);
+  }
+  setFetching(false);
+}
+
+function searchStudent(
+  searchText,
+  filteredStudents,
+  students,
+  setFilteredStudents
+) {
+  if (searchText == "") {
+    setFilteredStudents(students);
+    return;
+  }
+  filteredStudents = [];
+  students.forEach((student) => {
+    if (
+      student.personName.toLowerCase().includes(searchText.toLowerCase()) ||
+      student.userId.toLowerCase().includes(searchText.toLowerCase())
+    ) {
+      filteredStudents.push(student);
+    }
+  });
+  setFilteredStudents(filteredStudents);
+}
+
+export default AddFacultyModal;
--- pages/admin/create-course.js
+++ pages/admin/create-course.js
@@ -1,76 +1,112 @@
 import { useReducer, useState } from "react";
 import style from "./form.module.css";
 import AddStudentModal from "../admin/add-student-modal";
-import { StudentsContext, StudentsDispatchContext } from "../utils/contexts";
+import {
+  FacultiesContext,
+  FacultiesDispatchContext,
+  StudentsContext,
+  StudentsDispatchContext,
+} from "../utils/contexts";
+import AddFacultyModal from "./add-faculty-modal";
 
 export default function CreateCourse() {
   const [addStudent, setAddStudent] = useState(false);
-  const [students, dispatch] = useReducer(studentReducer, []);
+  const [addFaculty, setAddFaculty] = useState(false);
+  const [students, studentDispatch] = useReducer(studentReducer, []);
+  const [faculty, facultyDispatch] = useReducer(facultyReducer, []);
   return (
-    <StudentsDispatchContext.Provider value={dispatch}>
-      <StudentsContext.Provider value={students}>
-        <div className={style.font}>
-          <div className="h-screen bg-neutral-900 w-96 pl-6 pr-6 pt-4 border-neutral-800 border-solid border-x-2">
-            <div>
-              <div className="font-extrabold text-2xl text-neutral-200">
-                Create Course
-              </div>
-              <div className="text-xs text-neutral-500 font-regular pt-2">
-                Enter course details
-              </div>
-              <div className="pt-6">
-                <div className="flex flex-col">
-                  <div className="text-neutral-400 text-xs font-medium pb-2 placeholder:text-neutral-300">
-                    Course Name
+    <FacultiesDispatchContext.Provider value={facultyDispatch}>
+      <FacultiesContext.Provider value={faculty}>
+        <StudentsDispatchContext.Provider value={studentDispatch}>
+          <StudentsContext.Provider value={students}>
+            <div className={style.font}>
+              <div className="h-screen bg-neutral-900 w-96 pl-6 pr-6 pt-4 border-neutral-800 border-solid border-x-2">
+                <div>
+                  <div className="font-extrabold text-2xl text-neutral-200">
+                    Create Course
                   </div>
-                  <input
-                    placeholder="Enter course name"
-                    className="placeholder:text-neutral-600 max-w-full bg-neutral-900 rounded-md w-full h-10 border-2 border-neutral-800 focus:ring-yellow-400 focus:ring-2 focus:outline-none text-gray-300 font-regular text-sm p-2"
-                  ></input>
-                  <div className="text-neutral-400 text-xs font-medium pb-2 pt-5">
-                    Course ID
+                  <div className="text-xs text-neutral-500 font-regular pt-2">
+                    Enter course details
                   </div>
-                  <input
-                    placeholder="Enter course ID"
-                    className="placeholder:text-neutral-600 max-w-full bg-neutral-900 rounded-md w-full h-10 border-2 border-neutral-800 focus:ring-yellow-400 focus:ring-2 focus:outline-none text-gray-300 font-regular text-sm p-2"
-                  ></input>
-                  {/* <div className="text-neutral-400 text-xs font-medium pb-2 pt-5">Year of Enrollment</div> */}
-                  {/* <input placeholder="Enter your password" className="placeholder:text-neutral-600 max-w-full bg-neutral-900 rounded-md w-full h-10 border-2 border-neutral-800 focus:ring-yellow-400 focus:ring-2 focus:outline-none text-gray-300 font-regular text-sm p-2"></input> */}
-                  {/* <div className="text-neutral-400 text-xs font-medium pb-2 pt-5">
+                  <div className="pt-6">
+                    <div className="flex flex-col">
+                      <div className="text-neutral-400 text-xs font-medium pb-2 placeholder:text-neutral-300">
+                        Course Name
+                      </div>
+                      <input
+                        placeholder="Enter course name"
+                        className="placeholder:text-neutral-600 max-w-full bg-neutral-900 rounded-md w-full h-10 border-2 border-neutral-800 focus:ring-yellow-400 focus:ring-2 focus:outline-none text-gray-300 font-regular text-sm p-2"
+                      ></input>
+                      <div className="text-neutral-400 text-xs font-medium pb-2 pt-5">
+                        Course ID
+                      </div>
+                      <input
+                        placeholder="Enter course ID"
+                        className="placeholder:text-neutral-600 max-w-full bg-neutral-900 rounded-md w-full h-10 border-2 border-neutral-800 focus:ring-yellow-400 focus:ring-2 focus:outline-none text-gray-300 font-regular text-sm p-2"
+                      ></input>
+                      {/* <div className="text-neutral-400 text-xs font-medium pb-2 pt-5">Year of Enrollment</div> */}
+                      {/* <input placeholder="Enter your password" className="placeholder:text-neutral-600 max-w-full bg-neutral-900 rounded-md w-full h-10 border-2 border-neutral-800 focus:ring-yellow-400 focus:ring-2 focus:outline-none text-gray-300 font-regular text-sm p-2"></input> */}
+                      {/* <div className="text-neutral-400 text-xs font-medium pb-2 pt-5">
                   Student ID
                 </div>
                 <input
                   placeholder="Enter your password"
                   className="placeholder:text-neutral-600 max-w-full bg-neutral-900 rounded-md w-full h-10 border-2 border-neutral-800 focus:ring-yellow-400 focus:ring-2 focus:outline-none text-gray-300 font-regular text-sm p-2"
                 ></input> */}
-                  {/* <input class="border border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent ..."></input> */}
-                  {/* <div className="p-3 rounded-xl mt-6 bg-gradient-to-r duration-300 hover:rounded-md bg-yellow-400">
+                      {/* <input class="border border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent ..."></input> */}
+                      {/* <div className="p-3 rounded-xl mt-6 bg-gradient-to-r duration-300 hover:rounded-md bg-yellow-400">
                         <div className="font-extrabold text-black text-center cursor-pointer">Log In</div>
                     </div> */}
-                  {/* <div className="text-neutral-600  font-medium text-center pt-4 text-xs">Account doesn't exist? <span className="text-yellow-600 cursor-pointer text-xs">Contact Admin.</span></div> */}
+                      {/* <div className="text-neutral-600  font-medium text-center pt-4 text-xs">Account doesn't exist? <span className="text-yellow-600 cursor-pointer text-xs">Contact Admin.</span></div> */}
 
-                  <div className="text-xs font-bold pt-6 text-yellow-400 hover:text-yellow-600 duration-300 cursor-pointer">
-                    Add Faculty
+                      <div
+                        onClick={() => setAddFaculty(true)}
+                        className="text-xs font-bold pt-6 text-yellow-400 hover:text-yellow-600 duration-300 cursor-pointer"
+                      >
+                        Add Faculty
+                      </div>
+                      <div
+                        onClick={() => setAddStudent(true)}
+                        className="text-xs font-bold pt-4 text-yellow-400 hover:text-yellow-600 duration-300 cursor-pointer"
+                      >
+                        Add Students
+                      </div>
+                      <AddStudentModal
+                        show={addStudent}
+                        onClose={() => setAddStudent(false)}
+                      />
+                      <AddFacultyModal
+                        show={addFaculty}
+                        onClose={() => setAddFaculty(false)}
+                      />
+                    </div>
                   </div>
-                  <div
-                    onClick={() => setAddStudent(true)}
-                    className="text-xs font-bold pt-4 text-yellow-400 hover:text-yellow-600 duration-300 cursor-pointer"
-                  >
-                    Add Students
-                  </div>
-                  <AddStudentModal
-                    show={addStudent}
-                    onClose={() => setAddStudent(false)}
-                  />
                 </div>
+                <div></div>
               </div>
             </div>
-            <div></div>
-          </div>
-        </div>
-      </StudentsContext.Provider>
-    </StudentsDispatchContext.Provider>
+          </StudentsContext.Provider>
+        </StudentsDispatchContext.Provider>
+      </FacultiesContext.Provider>
+    </FacultiesDispatchContext.Provider>
   );
+}
+
+function facultyReducer(faculty, action) {
+  switch (action.type) {
+    case "add": {
+      return [
+        ...faculty,
+        {
+          userId: action.userId,
+          personName: action.personName,
+        },
+      ];
+    }
+    case "remove": {
+      return faculty.filter((f) => f.userId != action.userId);
+    }
+  }
 }
 
 function studentReducer(students, action) {
