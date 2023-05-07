464faa0c87c3699b9645d28ca0d20cce5bc99839
Samuel Khongthaw <skthaw1@gmail.com>
2023-05-07 19:57:21 +0530

    added attendance marking panel

--- pages/faculty/course/[course].js
+++ pages/faculty/course/[course].js
@@ -1,7 +1,41 @@
 import { useRouter } from "next/router";
+import style from "../font.module.css";
+import Attendance from "./attendance";
 
 export default function Course() {
   const router = useRouter();
   const { course } = router.query;
-  return <div></div>;
+  return (
+    <div className={style.font}>
+      <div className="flex flex-col justify-between h-screen  p-6">
+        <div className="flex justify-between border-b-2 h-screen">
+          <div className="border-r-2 w-screen">
+            <div className="font-bold pb-5">New Attendance</div>
+            <div className="text-neutral-500 text-sm">Select date</div>
+          </div>
+          <div>
+            <div className="pb-4 pl-14 flex flex-col items-center h-96">
+              <div className="pb-5">Previous Attendance Data</div>
+              <div className="overflow-y-scroll w-56">
+                <Attendance />
+                <Attendance />
+                <Attendance />
+                <Attendance />
+                <Attendance />
+                <Attendance />
+                <Attendance />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="pt-5">
+          <div>Course Statistics</div>
+          <div>
+            <div className="text-neutral-500">40 students enrolled</div>
+            <div className="text-neutral-500">10 classes taken</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ pages/faculty/course/attendance.js
@@ -0,0 +1,8 @@
+export default function Attendance() {
+  return (
+    <div className="bg-neutral-800 p-4 mb-2 rounded-lg w-max">
+      <div>Date: 06/09/2001</div>
+      <div className="text-neutral-300 text-xs">34 student present</div>
+    </div>
+  );
+}
